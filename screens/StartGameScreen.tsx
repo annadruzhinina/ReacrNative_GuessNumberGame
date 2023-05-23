9b0356aa356008a72bbc7dd5070431b8a776fe72
anna <anna@rcbd.org>
2023-05-22 22:40:11 -0400

    Styling for Android & iOS Shadow

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import {TextInput, View, Text} from 'react-native';
+import {TextInput, View, StyleSheet} from 'react-native';
 
 import PrimaryButton from '../components/PrimaryButton';
 
 export default function StartScreen() {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4, //shadow for android property
+    shadowColor: 'black', //for ios
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
